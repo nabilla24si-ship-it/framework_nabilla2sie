ae69a74ba469f8b25658a1b56a4815925ee625ec
nabilla24si-ship-it <nabilla24sii@pcr.ac.id>
2025-09-28 22:24:54 +0700

    terakhir

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\MahasiswaController; // inisialisasi class Controller
-
+use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 
 Route::get('/', function () {
@@ -11,37 +10,32 @@ Route::get('/', function () {
 });
 
 Route::get('/pcr', function () {
-    return 'Selamat Datang di Website Kampus PCR!'; //ini adalah besic Route
+    return 'Selamat Datang di Website Kampus PCR!';
 });
 
-Route::get('/mahasiswa', function () {  //ini adalah besic Route dengan nama route berbeda
+Route::get('/mahasiswa', function () {
     return 'Halo Mahasiswa';
 });
 
-Route::get('/nama/{nama_saya}', function ($nama_saya) {   //code route parameter menamplkan nama saya
-    return 'Nama saya: '.$nama_saya;
+Route::get('/nama/{nama_saya}', function ($nama_saya) {
+    return 'Nama saya: ' . $nama_saya;
 });
 
-//parameter bersifat opsional dan jika parameter tidak ada tidak not foud
 Route::get('/nim/{nim_saya?}', function ($nim_saya = '') {
-    return 'NIM saya: '.$nim_saya;
+    return 'NIM saya: ' . $nim_saya;
 });
 
-//named Route dengan membuat link di view
 Route::get('/mahasiswa', function () {
     return 'Halo Mahasiswa';
 })->name('mahasiswa.show');
 
-//route yang mengarahkan ke MahasiswaController dan function show()
-Route ::get('/mahasiswa/{paraml}', [MahasiswaController::class, 'show']);
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
 
 Route::get('/about', function () {
-    return view('halaman-about'); //ini route halaman about
+    return view('halaman-about');
 });
 
-//Latihan
-//ini adalah controler mata kuliah
 Route::get('/matakuliah', [MatakuliahController::class, 'index']);
 Route::get('/matakuliah/show/{kode?}', [MatakuliahController::class, 'show']);
 
-/////
+Route::get('/home', [HomeController::class, 'index']);
