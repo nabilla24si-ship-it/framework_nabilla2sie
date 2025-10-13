191447ef7524e2ac2da021db5534de1de1c57e7f
nabilla24si-ship-it <nabilla24si@mahasiswa.pcr.ac.id>
2025-10-13 22:43:30 +0700

    template

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
-use App\Http\Controllers\QuestionController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -43,3 +44,8 @@ Route::get('/home', [HomeController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
